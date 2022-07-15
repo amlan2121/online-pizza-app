72c6f7bda7ad94bf5313bb8e9fa81e8abe69d7b5
amlan2121 <51921355+amlan2121@users.noreply.github.com>
2022-07-15 12:17:22 +0530

    Add files via upload

---
+++ src/main/java/com/cg/onlinepizzaapp/onlinepizzaapp/exception/CouponIdNotFoundException.java
@@ -0,0 +1,10 @@
+package com.cg.onlinepizzaapp.onlinepizzaapp.exception;
+
+public class CouponIdNotFoundException extends Exception {
+	
+	public CouponIdNotFoundException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+}
