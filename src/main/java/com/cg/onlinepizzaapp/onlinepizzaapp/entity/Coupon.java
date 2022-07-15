6304699bb451e38069f201f3dba85bebaa453477
amlan2121 <51921355+amlan2121@users.noreply.github.com>
2022-07-15 12:16:31 +0530

    Add files via upload

---
+++ src/main/java/com/cg/onlinepizzaapp/onlinepizzaapp/entity/Coupon.java
@@ -0,0 +1,55 @@
+package com.cg.onlinepizzaapp.onlinepizzaapp.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class Coupon {
+	@Id
+	private int couponId;
+	private String couponName;
+	private String couponType;
+	private String couponDescription;
+	private int couponPizzaId;
+
+	public int getCouponId() {
+		return couponId;
+	}
+
+	public void setCouponId(int couponId) {
+		this.couponId = couponId;
+	}
+
+	public String getCouponName() {
+		return couponName;
+	}
+
+	public void setCouponName(String couponName) {
+		this.couponName = couponName;
+	}
+
+	public String getCouponType() {
+		return couponType;
+	}
+
+	public void setCouponType(String couponType) {
+		this.couponType = couponType;
+	}
+
+	public String getCouponDescription() {
+		return couponDescription;
+	}
+
+	public void setCouponDescription(String couponDescription) {
+		this.couponDescription = couponDescription;
+	}
+
+	public int getCouponPizzaId() {
+		return couponPizzaId;
+	}
+
+	public void setCouponPizzaId(int couponPizzaId) {
+		this.couponPizzaId = couponPizzaId;
+	}
+
+}
