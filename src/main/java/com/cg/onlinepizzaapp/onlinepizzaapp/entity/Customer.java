0f73d522ce4a59572592fe8d43f3d3c59086e60d
Priyabrata-007 <109204035+Priyabrata-007@users.noreply.github.com>
2022-07-15 11:38:13 +0530

    Add files via upload

---
+++ src/main/java/com/cg/onlinepizzaapp/onlinepizzaapp/entity/Customer.java
@@ -0,0 +1,87 @@
+package com.cg.onlinepizzaapp.onlinepizzaapp.entity;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+@Entity
+
+public class Customer{
+	@OneToOne(cascade = CascadeType.ALL)
+	private User user;
+	@Id
+	private int customerId;
+	
+	private String customerName;
+	private long customerMobile;
+	private String customerEmail;
+	private String customerAddress;
+	
+	public Customer() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	
+	
+	
+	
+	
+	public Customer(User user, int customerId, String customerName, long customerMobile, String customerEmail,
+			String customerAddress) {
+		super();
+		this.user = user;
+		this.customerId = customerId;
+		this.customerName = customerName;
+		this.customerMobile = customerMobile;
+		this.customerEmail = customerEmail;
+		this.customerAddress = customerAddress;
+	}
+
+
+
+
+
+	public int getCustomerId() {
+		return customerId;
+	}
+	public void setCustomerId(int customerId) {
+		this.customerId = customerId;
+	}
+	public String getCustomerName() {
+		return customerName;
+	}
+	public void setCustomerName(String customerName) {
+		this.customerName = customerName;
+	}
+	public long getCustomerMobile() {
+		return customerMobile;
+	}
+	public void setCustomerMobile(long customerMobile) {
+		this.customerMobile = customerMobile;
+	}
+	public String getCustomerEmail() {
+		return customerEmail;
+	}
+	public void setCustomerEmail(String customerEmail) {
+		this.customerEmail = customerEmail;
+	}
+	public String getCustomerAddress() {
+		return customerAddress;
+	}
+	public void setCustomerAddress(String customerAddress) {
+		this.customerAddress = customerAddress;
+	}
+	
+	public User getUser() {
+		return user;
+	}
+	public void setUser(User user) {
+		this.user = user;
+	}
+	
+	
+}
