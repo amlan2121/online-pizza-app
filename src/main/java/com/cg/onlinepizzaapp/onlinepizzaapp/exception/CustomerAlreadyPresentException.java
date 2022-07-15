993d093859e312b8bceea75bc2e8507b87c0bbd3
Priyabrata-007 <109204035+Priyabrata-007@users.noreply.github.com>
2022-07-15 11:42:37 +0530

    Add files via upload

---
+++ src/main/java/com/cg/onlinepizzaapp/onlinepizzaapp/exception/CustomerAlreadyPresentException.java
@@ -0,0 +1,10 @@
+package com.cg.onlinepizzaapp.onlinepizzaapp.exceptions;
+
+public class CustomerAlreadyPresentException extends RuntimeException {
+	public CustomerAlreadyPresentException(String message) {
+		super(message);
+		
+
+	}
+
+}
---
+++ src/main/java/com/cg/onlinepizzaapp/onlinepizzaapp/exception/CustomerIdNotFoundException.java
@@ -0,0 +1,9 @@
+package com.cg.onlinepizzaapp.onlinepizzaapp.exceptions;
+
+public class CustomerIdNotFoundException extends RuntimeException {
+	public CustomerIdNotFoundException(String message) {
+		super(message);
+
+	}
+
+}
