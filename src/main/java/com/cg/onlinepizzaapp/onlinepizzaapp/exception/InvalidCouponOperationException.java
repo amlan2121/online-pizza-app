a948723299d5c6eca6b33c4048b81bef4a831f2f
Swagat0810 <82587453+Swagat0810@users.noreply.github.com>
2022-07-18 09:19:46 +0530

    Delete InvalidCouponOperationException.java

--- src/main/java/com/cg/onlinepizzaapp/onlinepizzaapp/exception/InvalidCouponOperationException.java
+++
@@ -1,9 +0,0 @@
-package com.cg.onlinepizzaapp.onlinepizzaapp.exception;
-
-public class InvalidCouponOperationException extends Exception{
-	public InvalidCouponOperationException(String message) {
-		super(message);
-		// TODO Auto-generated constructor stub
-	}
-
-}
