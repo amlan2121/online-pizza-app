c66b6b3c044bd971ed0f9a2921b108e899b8bf12
Swagat0810 <82587453+Swagat0810@users.noreply.github.com>
2022-07-18 09:21:22 +0530

    Add files via upload

---
+++ src/main/java/com/cg/onlinepizzaapp/onlinepizzaapp/exception/CustomerAlreadyPresentException.java
@@ -0,0 +1,10 @@
+package com.cg.onlinepizzaapp.onlinepizzaapp.exception;
+
+public class CustomerAlreadyPresentException extends RuntimeException {
+	public CustomerAlreadyPresentException(String message) {
+		super(message);
+		
+
+	}
+
+}
